8697610b4f739790103e9d900023008c94302c30
Haru-143 <143gaeulpink@gmail.com>
2025-05-04 11:58:17 +0800

    Add function for playing a single round and outputing result

--- script.js
+++ script.js
@@ -19,3 +19,49 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Enter Rock, Paper, or Scissor").toLowerCase();
 }
+
+// Function for playing a single round
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'rock') {
+        console.log("Computer chose rock.");
+        console.log("It's a tie!");
+        return;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log("Computer chose rock.");
+        console.log("You win!");
+        return;
+    } else if (humanChoice === 'scissor' && computerChoice === 'rock') {
+        console.log("Computer chose rock.");
+        console.log("You lose!");
+        return;
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log("Computer chose paper.");
+        console.log("You lose!");
+        return;
+    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+        console.log("Computer chose paper.");
+        console.log("It's a tie!");
+        return;
+    } else if (humanChoice === 'scissor' && computerChoice === 'paper') {
+        console.log("Computer chose paper.");
+        console.log("You win!");
+        return;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissor') {
+        console.log("Computer chose scissor.");
+        console.log("You win!");
+        return;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissor') {
+        console.log("Computer chose scissor.");
+        console.log("You lose!");
+        return;
+    } else if (humanChoice === 'scissor' && computerChoice === 'scissor') {
+        console.log("Computer chose scissor.");
+        console.log("It's a tie!");
+        return;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
